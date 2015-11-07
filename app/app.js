793a3c300976ec8e87493174a25d04fca6594cc9
Dimka <obnemashge@gmail.com>
2015-11-07 13:00:29 +0300

    test commit

--- app/app.js
+++ app/app.js
@@ -7,17 +7,6 @@
         .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
             $locationProvider.html5Mode(true);
-            //
-            //$routeProvider.when('/users', {
-            //    controller: 'usersController',
-            //    templateUrl: 'app/users/users.html',
-            //    resolve: {
-            //        initUsers: ['usersService', function (usersService) {
-            //            return usersService.getUsers();
-            //        }]
-            //    }
-            //});
-
             $routeProvider.otherwise({ redirectTo: '/' });
         }]);
 })();
