de0a1cab13e676879063138ccf0d4ec68652b4e3
dmitry.vanin <dmitry.vanin@monterosa.co.uk>
2016-03-17 12:28:07 +0300

    fb api demo

--- app/components/mainPageController.js
+++ app/components/mainPageController.js
@@ -7,21 +7,22 @@
 	function mainPageController(mainPageService, $state) {
 		var vm = this;
 
-		vm.showNavigation = false;
+		vm.isAuthorised = false;
 		vm.fbLogin = fbLogin;
+
 
 		init();
 
 		function init(){
-			//FB.getLoginStatus(function (response) {
-			//	if (response.status === 'connected') {
-			//		$state.go('userInfo');
-			//		console.log('connected');
-			//	} else {
-			//		console.log('disconnected');
-			//		$state.go('loginFB');
-			//	}
-			//});
+			FB.getLoginStatus(function (response) {
+				if (response.status === 'connected') {
+					vm.isAuthorised = true;
+					console.log('connected');
+				} else {
+					vm.isAuthorised = false;
+					console.log('disconnected');
+				}
+			});
 		}
 
 		function fbLogin() {
