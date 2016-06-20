2f728a62812e59dac2a8693567c257e018135579
dmitry.vanin <dmitry.vanin@monterosa.co.uk>
2016-06-20 18:19:03 +0300

    big changes for live demo 2

---
+++ app/services/fbService.js
@@ -0,0 +1,57 @@
+(function () {
+	'use strict';
+	angular
+		.module('app')
+		.factory('facebookService', ['$window','$q', 'appConfig', facebookService]);
+
+	function facebookService($window, $q, config) {
+		var facebook = $window.FB;
+
+		return {
+			init: init,
+			getLoginStatus: getLoginStatus,
+			login: login
+		};
+
+		function init() {
+			var defer = $q.defer();
+
+			facebook.init({
+				appId: config.FB_APP_ID,
+				//status: true,
+				xfbml: true,
+				version: 'v2.6'
+			});
+
+			setTimeout(function(){
+				defer.resolve();
+			},1000);
+
+			return defer.promise;
+		}
+
+		function getLoginStatus() {
+			var deferred = $q.defer();
+
+			facebook.getLoginStatus(function(response){
+				console.log(response)
+			});
+
+			return deferred.promise;
+		}
+
+		function login() {
+			var deferred = $q.defer();
+
+			facebook.login(function (response) {
+				if (!response || response.error) {
+					deferred.reject('Error occured');
+				} else {
+					deferred.resolve(response);
+				}
+			}, {scope: 'publish_actions,user_friends'});
+
+			return deferred.promise;
+		}
+	}
+})();
