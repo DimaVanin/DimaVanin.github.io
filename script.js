8f8ff89e41c79aeb9a7fd60c295267614d352e9b
dmitry.vanin <dmitry.vanin@monterosa.co.uk>
2016-07-15 12:34:17 +0300

    fex fex

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
-//$(function () {
-//	var iframe = $('#iframe');
-//
-//
-//	$('#btn-inc').on('click', function () {
-//		iframe.height(1.1 * iframe.height());
-//	});
-//
-//	$('#btn-dec').on('click', function () {
-//		iframe.height(.9 * iframe.height());
-//	});
-//});
-
 $(function () {
-	var way = false;
-	var timer;
-	var percent = 0;
-	var progress = $('.progress');
+	var iframeW = $('#iframe-wrapper');
 
-	function renderBar() {
-		progress.width(percent + '%');
-	}
+	$('#btn-inc').on('click', function () {
+		iframeW.height(1.1 * iframeW.height());
+	});
 
-	function inc() {
-		percent++;
-		percent++;
-
-		if (percent < 100) {
-			renderBar();
-		} else {
-			tadam()
-		}
-	}
-
-	function dec() {
-		percent--;
-		percent--;
-
-		if (percent > 0) {
-			renderBar();
-		} else {
-			tadam()
-		}
-	}
-
-	function tadam() {
-		clearInterval(timer);
-
-		if (way = !way) {
-			timer = setInterval(inc, 100)
-		} else {
-			timer = setInterval(dec, 100)
-		}
-	}
-
-	tadam();
+	$('#btn-dec').on('click', function () {
+		iframeW.height(.9 * iframeW.height());
+	});
 });
 
+//$(function () {
+//	var way = false;
+//	var timer;
+//	var percent = 0;
+//	var progress = $('.progress');
+//
+//	function renderBar() {
+//		progress.width(percent + '%');
+//	}
+//
+//	function inc() {
+//		percent++;
+//		percent++;
+//
+//		if (percent < 100) {
+//			renderBar();
+//		} else {
+//			tadam()
+//		}
+//	}
+//
+//	function dec() {
+//		percent--;
+//		percent--;
+//
+//		if (percent > 0) {
+//			renderBar();
+//		} else {
+//			tadam()
+//		}
+//	}
+//
+//	function tadam() {
+//		clearInterval(timer);
+//
+//		if (way = !way) {
+//			timer = setInterval(inc, 100)
+//		} else {
+//			timer = setInterval(dec, 100)
+//		}
+//	}
+//
+//	tadam();
+//});
+//
